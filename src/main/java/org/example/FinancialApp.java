80d9a4657e64e19afac235cc4569f685d39c4a0b
atortpot <114068050+atortpot@users.noreply.github.com>
2024-05-29 16:12:57 +0200

    Main

---
+++ src/main/java/org/example/FinancialApp.java
@@ -0,0 +1,16 @@
+package org.example;
+public class FinancialApp {
+    public static void main(String[] args) {
+        FinancialModel model = new FinancialModel();
+        FinancialView view = new FinancialView();
+        FinancialController controller = new FinancialController(model, view);
+
+        controller.updateStockPrice("NASDAQ", 150.50);
+        controller.updateStockPrice("NYSE", 145.75);
+        controller.showMarketValue(1000000);
+        controller.showPriceToBookRatio(5000000.00, 1000000);
+    }
+}
+
+
+
