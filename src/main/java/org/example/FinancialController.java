7e11ab185dfa693003f4ca699a137318c99cfe82
atortpot <114068050+atortpot@users.noreply.github.com>
2024-05-29 16:11:46 +0200

    Controlador

---
+++ src/main/java/org/example/FinancialController.java
@@ -0,0 +1,26 @@
+package org.example;
+public class FinancialController {
+    private FinancialModel model;
+    private FinancialView view;
+
+    public FinancialController(FinancialModel model, FinancialView view) {
+        this.model = model;
+        this.view = view;
+    }
+
+    public void updateStockPrice(String market, double price) {
+        model.setStockPrice(market, price);
+        view.displayStockPrice(market, price);
+    }
+
+    public void showMarketValue(int sharesOutstanding) {
+        double marketValue = model.calculateMarketValue(sharesOutstanding);
+        view.displayMarketValue(marketValue);
+    }
+
+    public void showPriceToBookRatio(double bookValue, int sharesOutstanding) {
+        double ratio = model.calculatePriceToBookRatio(bookValue, sharesOutstanding);
+        view.displayPriceToBookRatio(ratio);
+    }
+}
+
